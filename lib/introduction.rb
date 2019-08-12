92a8cccaa0175d972206c3dd21fb6460edce0a7d
clean-command-1851 <ed7901979t@gmail.com>
2019-08-12 23:54:06 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,5 @@
 #defining function    
-def introduction(name)
-    puts "Hi, my name is #{name}."
+def introduction(name, language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
+introduction("Edward", "Ruby")
